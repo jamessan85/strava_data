faed058348244abedbe7f355bb9d04c22da1c4c3
James Sanderson <jamessandersoon@gmail.com>
2017-05-21 08:25:45 +0100

    Connect to MongoDb

--- strava_data.py
+++ strava_data.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 import json
+import os
 
 app = Flask(__name__)
+
+MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
+DBS_NAME = os.getenv('MONGO_DB_NAME', 'strava')
 
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
@@ -47,7 +51,7 @@ def strava_data():
 
     # Open a connection to MongoDB using a with statement such that the
     # connection will be closed as soon as we exit the with statement
-    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+    with MongoClient(MONGO_URI) as conn:
         # Define which collection we wish to access
         collection = conn[DBS_NAME][COLLECTION_NAME]
         # Retrieve a result set only with the fields defined in FIELDS
